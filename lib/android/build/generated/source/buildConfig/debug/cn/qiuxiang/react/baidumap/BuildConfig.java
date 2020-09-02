7506303923c52f835497171c477d0a9a3548a3e8
zhoujian <zhoujj-f@glodon.com>
2020-09-02 10:14:22 +0800

    fork https://github.com/qiuxiang/react-native-baidumap-sdk

---
+++ lib/android/build/generated/source/buildConfig/debug/cn/qiuxiang/react/baidumap/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.qiuxiang.react.baidumap;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "cn.qiuxiang.react.baidumap";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "cn.qiuxiang.react.baidumap";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
